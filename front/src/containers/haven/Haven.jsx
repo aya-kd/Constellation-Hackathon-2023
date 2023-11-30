07761b09e9feec85a88463401766d363d285e3dc
chaima2911 <chaimadaas05@gmail.com>
2023-11-30 09:49:16 +0100

    initial commit

---
+++ front/src/containers/haven/Haven.jsx
@@ -0,0 +1,32 @@
+/*import React from 'react'
+import './haven.css'
+import {  Group_13 } from './imports';
+
+const Haven = () => {
+  return (
+    <div className="haven__haven section__margin" id="our achievements">
+      <div className="haven__haven-title">
+        <h3>Our Achievements</h3>
+      </div>
+      <div className="haven__haven-images">
+        <div className="haven__haven-images-all">
+          <img src={Group_13} alt="PGroup_13" className="haven__haven-images-all-image1"/>
+        </div>
+        <div className="haven__haven-images-content1">
+          <p className="haven__haven-images-content1-text1">3M $</p>
+          <h3 className="haven__haven-images-content1-title1"> Collected Donations</h3>
+        </div>
+        <div className="haven__haven-images-content2">
+          <p className="haven__haven-images-content2-text2">150K</p>
+          <h3 className="haven__haven-images-content1-title2">Helped Refugees</h3>
+        </div>
+        <div className="haven__haven-images-content3">
+          <p className="haven__haven-images-content3-text3">82K</p>
+          <h3 className="haven__haven-images-content1-title3">Listed Houses</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Haven*/
