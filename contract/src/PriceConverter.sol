2a369f96ad2e53540b04ee9b101595096fae83bb
Aya <ayakhedda98@gmail.com>
2023-12-10 23:58:27 +0100

    update

--- contract/src/PriceConverter.sol
+++ contract/src/PriceConverter.sol
@@ -9,9 +9,7 @@ library PriceConverter {
     function getPrice() internal view returns(uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (,int256 price,,,) = priceFeed.latestRoundData();
-        //Price of ETH in terms of USD
-        // 2077_00000000 = 2077 * 1e8 = 2077$ * 1e18
-        return uint256(price * 1e10); //to make it 2077 * amount of wei
+        return uint256(price * 1e10); 
     }
 
     function usdToEth(uint _amount) internal view returns (uint) {
